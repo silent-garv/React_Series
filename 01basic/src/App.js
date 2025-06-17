3b37632d17464d5c54614f58875577bbcb6d0cb4
Garv Pankaj <garvpankaj22@gmail.com>
2025-06-17 22:30:10 +0530

    react day 01

---
+++ 01basic/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>namaste Duniya mai garv </h1>
+  );
+}
+
+export default App;
---
+++ 01reactbyvite/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+   <h1>Namaste Duniya main garv pankaj</h1>
+  )
+}
+
+export default App
