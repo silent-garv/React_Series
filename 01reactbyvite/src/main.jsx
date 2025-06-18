7ffcf57eca8f8a3cf4fbe8fe2a6540b9f84605f8
Garv Pankaj <garvpankaj22@gmail.com>
2025-06-18 13:27:10 +0530

    day 02

--- 01reactbyvite/src/main.jsx
+++ 01reactbyvite/src/main.jsx
@@ -2,8 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function  MyApp(){
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  )
+
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <MyApp />
   </StrictMode>
 )
